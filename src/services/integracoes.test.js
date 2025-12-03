25994a2dd49f11ccdce0a9413a41d56519c68398
Matheus Tavares <matheustavares1a@gmail.com>
2025-12-03 13:35:47 -0300

    Testing requests with errors

--- src/services/integracoes.test.js
+++ src/services/integracoes.test.js
@@ -21,11 +21,26 @@ const mockRequisicao = (retorno) => {
     });
 };
 
+const mockRequisicaoErro = () => {
+    return new Promise((_, reject) => {
+        setTimeout(() => {
+            reject();
+        }, 200);
+    });
+};
+
 describe('Requisições para a API', () => {
     test('Deve retornar uma lista de transações', async () => {
         api.get.mockImplementation(() => mockRequisicao(mockTransacao));
 
         const transacoes = await buscaTransacoes();
         expect(transacoes).toEqual(mockTransacao);
+    });
+
+    test('Deve retornar uma lista vazia quando a requisição falhar', async () => {
+        api.get.mockImplementation(() => mockRequisicaoErro());
+
+        const transacoes = await buscaTransacoes();
+        expect(transacoes).toEqual([]);
     });
 });
